75cf8283cf72bc277677b3e3c941975d01d89055
tverdoy <tverdoy.work@gmail.com>
2023-10-13 14:12:47 +0300

    Add findArguments

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var supportOperations = []string{"+", "-", "/", "*"}
+
 func main() {
-	parse("1 + 2")
+	parse("1 + 2 + 3")
 }
 
 func parse(input string) {
-	println(input)
+	firstArguments, secondArgument, err := findArguments(input)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(firstArguments, secondArgument)
+}
+
+func findArguments(input string) (string, string, error) {
+	input = strings.Replace(input, " ", "", -1)
+	for _, operation := range supportOperations {
+		splitInput := strings.Split(input, operation)
+		if len(splitInput) == 2 {
+			return splitInput[0], splitInput[1], nil
+		}
+	}
+
+	return "", "", errors.New("Syntax error")
 }
